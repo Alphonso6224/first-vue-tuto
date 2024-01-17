f0be97bb0ee68d7f8a8e1d3f04266cce0a78817d
AYMAR ADJAHO <aymar.adjaho@chem.loc>
2024-01-17 19:08:17 +0100

    Felicitation; utilisation de js-confetti opérationnel

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,37 @@
 <script setup>
 import { ref } from 'vue';
+import JSConfetti from 'js-confetti'
 import ChildComp from './components/ChildComp.vue'
+
+
+const confetti = new JSConfetti()
+
+function showConfetti() {
+  confetti.addConfetti()
+}
 
 const greating = ref('Hello du parent');
 const childMsg = ref('Pas encore de message de l\'enfant')
+
+showConfetti()
 </script>
 
 <template>
+
+  <h1 @click="showConfetti">🎉 Félicitations!</h1>
   <!-- <ChildComp @response="(msg) => childMsg = msg" :msg="greating"/> -->
   <ChildComp>
     {{ greating }}
   </ChildComp>
   <p>{{ childMsg }}</p>
+
+
 </template>
 
 <style scoped>
-
+h1 {
+  text-align: center;
+  cursor: pointer;
+  margin-top: 3em;
+}
 </style>
