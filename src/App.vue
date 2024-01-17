b9b45040d4121a21d3e48060fd13dd0579011552
AYMAR ADJAHO <aymar.adjaho@chem.loc>
2024-01-17 17:40:42 +0100

    3. Liaisons d'attributs Ajout d'une liaison dynamique class au <h1>, en utilisant la propriété titleClass ref comme valeur

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
 import { ref } from 'vue';
 
+// Déclaration d'une référenche de chaîne
 const message = ref('Hello World');
+
+const titleClass = ref('title')
 
 </script>
 
 <template>
-  <h1>Rendez moi dynamique</h1>
-  <p>{{ message.split('').reverse().join('') }}</p>
+  <h1 :id="dynamicId" :class="titleClass">Passez moi en rouge</h1>
+  <p>{{ titleClass }}</p>
 </template>
+
+<style scoped>
+.title {
+  color: red;
+}
+</style>
