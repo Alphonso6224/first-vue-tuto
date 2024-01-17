b273287212d4f4ba8314e1c8974d9cb8f19885c8
AYMAR ADJAHO <aymar.adjaho@chem.loc>
2024-01-17 18:45:25 +0100

    Debut avec les props

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { ref } from 'vue';
 import ChildComp from './components/ChildComp.vue'
 
+const greating = ref('Hello du parent');
 </script>
 
 <template>
-  <ChildComp />
+  <ChildComp :msg="greating"/>
 </template>
 
 <style scoped>
--- src/components/ChildComp.vue
+++ src/components/ChildComp.vue
@@ -1,3 +1,10 @@
 <template>
     <h2>Un composant enfant!</h2>
+    <p>{{ msg || 'Aucune prop passée pour le moment'}}</p>
 </template>
+
+<script setup>
+const props = defineProps({
+    msg: String
+})
+</script>
