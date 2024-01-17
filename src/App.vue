3961d8a85c44870cb0ea250323e5c836c23a9d70
AYMAR ADJAHO <aymar.adjaho@chem.loc>
2024-01-17 18:37:53 +0100

    Premier pas avec les composants

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,10 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
+import ChildComp from './components/ChildComp.vue'
 
-const todoId = ref(1);
-const todoData = ref(null);
-
-async function fetchData() {
-  todoData.value = null;
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
-  )
-  todoData.value = await res.json()
-}
-
-watch(todoId, (newId) => {
-  console.log(`new count is: ${newId}`);
-  fetchData();
-})
-
-fetchData()
 </script>
 
 <template>
-  <p>Id de la liste de tâches: {{ todoId }}</p>
-  <button @click="todoId++" :disabled="!todoData">
-    Récupérer la prochaine liste de tâches
-  </button>
-  <p v-if="!todoData">Chargement...</p>
-  <pre v-else>
-    {{ todoData }}
-  </pre>
+  <ChildComp />
 </template>
 
 <style scoped>
