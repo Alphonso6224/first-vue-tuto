7505ecee1a58925ae5135621183bfe7ab671011f
AYMAR ADJAHO <aymar.adjaho@chem.loc>
2024-01-17 17:09:38 +0100

    Tutoriel vue, Creation du projet effectif et rendu déclaratif effectif

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Hello World');
+
+</script>
+
+<template>
+  <h1>Rendez moi dynamique</h1>
+  <p>{{ message.split('').reverse().join('') }}</p>
+</template>
